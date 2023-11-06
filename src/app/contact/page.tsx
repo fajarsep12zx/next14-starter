d22673715643eec51c81a33371b7c83242492e7c
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2023-11-06 16:10:24 +0700

    Testing page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,40 @@
+import { cookies } from 'next/headers'
+import Image from 'next/image'
+import { RedirectType, redirect } from 'next/navigation'
+
+export default function About() {
+  const loginCookies = cookies().get('loginauth')
+
+  if (!loginCookies) {
+    redirect('/', RedirectType.push)
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            By{' '}
+            <Image
+              priority
+              alt="Vercel Logo"
+              className="dark:invert"
+              height={24}
+              src="/vercel.svg"
+              width={100}
+            />
+          </a>
+        </div>
+      </div>
+    </main>
+  )
+}
