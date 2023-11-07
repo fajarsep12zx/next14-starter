e2097ad0739615cf8ca7d9895d38f33504400b9e
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2023-11-07 13:04:03 +0700

    Update base color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
         />
       </div>
 
-      <Button>Test</Button>
+      <Button variant="default">Test</Button>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
